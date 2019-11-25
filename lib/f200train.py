8e036ba6525d52a3e6a82b1630a18ec06a2f8d1b
nhamey <nhamey@iinet.net.au>
2019-11-26 08:46:34 +1000

    Update f200train.py

--- lib/f200train.py
+++ lib/f200train.py
@@ -1,6 +1,6 @@
 
 # Metarace : Cycle Race Abstractions
-# Copyright (C) 2012  Nathan Fraser
+# Copyright (C) 2018  Neil Hamey
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
